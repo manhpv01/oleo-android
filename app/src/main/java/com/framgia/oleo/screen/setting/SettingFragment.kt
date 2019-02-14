42030cd068b41e82029634c01b1876f68075e6d3
bui.van.hieu <FRAMGIA\bui.van.hieu@b121668-pc.framgia.com>
2019-02-14 09:57:58 +0700

    handle event click in setting screen and logout account facebook google

--- app/src/main/java/com/framgia/oleo/screen/setting/SettingFragment.kt
+++ app/src/main/java/com/framgia/oleo/screen/setting/SettingFragment.kt
@@ -4,13 +4,23 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
 import androidx.databinding.DataBindingUtil
+import com.facebook.AccessToken
+import com.facebook.GraphRequest
+import com.facebook.HttpMethod
+import com.facebook.login.LoginManager
 import com.framgia.oleo.R
 import com.framgia.oleo.base.BaseFragment
 import com.framgia.oleo.databinding.FragmentSettingBinding
+import com.framgia.oleo.screen.login.LoginFragment
+import com.framgia.oleo.utils.extension.isCheckMultiClick
+import com.framgia.oleo.utils.extension.replaceFragment
 import com.framgia.oleo.utils.liveData.autoCleared
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.fragment_setting.*
 
-class SettingFragment : BaseFragment() {
+class SettingFragment : BaseFragment(), View.OnClickListener {
 
     private lateinit var viewModel: SettingViewModel
     private var binding by autoCleared<FragmentSettingBinding>()
@@ -23,9 +33,45 @@ class SettingFragment : BaseFragment() {
     }
 
     override fun setUpView() {
+        textViewLogOut.setOnClickListener(this)
     }
 
     override fun bindView() {
+    }
+
+    override fun onClick(v: View?) {
+        when (v!!.id) {
+            R.id.textViewLogOut -> if (isCheckMultiClick()) logOut()
+        }
+    }
+
+    private fun logOut() {
+        val builder = AlertDialog.Builder(activity!!, R.style.alertDialog)
+        builder.setMessage(getString(R.string.validate_log_out)).setTitle(getString(R.string.log_out))
+        builder.setPositiveButton(getString(R.string.ok)) { dialog, id ->
+            signOutFacebook()
+            signOutGoogle()
+            replaceFragment(R.id.containerMain, LoginFragment.newInstance(), false)
+        }
+        builder.setNegativeButton(getString(R.string.cancel)) { dialog, which -> dialog.dismiss() }
+        val dialog = builder.create()
+        dialog.setCanceledOnTouchOutside(false)
+        dialog.show()
+    }
+
+    private fun signOutFacebook() {
+        if (AccessToken.getCurrentAccessToken() == null) {
+            return //already logged out
+        }
+        GraphRequest(AccessToken.getCurrentAccessToken(),
+                "/me/permissions/",
+                null,
+                HttpMethod.DELETE,
+                GraphRequest.Callback { LoginManager.getInstance().logOut() }).executeAsync()
+    }
+
+    private fun signOutGoogle() {
+        FirebaseAuth.getInstance()?.signOut()
     }
 
     companion object {
